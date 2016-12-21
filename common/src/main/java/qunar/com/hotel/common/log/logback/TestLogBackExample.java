462ccf22e39d4e1d3c8d59003545af3cd5fe50eb
543405693@qq.com <543405693@qq.com>
2016-12-21 19:05:08 +0800

    'test'

--- common/src/main/java/qunar/com/hotel/common/log/logback/TestLogBackExample.java
+++ common/src/main/java/qunar/com/hotel/common/log/logback/TestLogBackExample.java
@@ -1,10 +1,29 @@
 package qunar.com.hotel.common.log.logback;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by shixian.zhen on 2016/12/20.
  */
 
+
 public class TestLogBackExample {
 
+    private Logger logger = LoggerFactory.getLogger(TestLogBackExample.class);
+
+    public void info (String printContent){
+        logger.info("logger level info {}", printContent);
+    }
+
+    public void error (String printContent){
+        logger.error("logger level error {}", printContent);
+    }
+
+    public static void main(String args[]){
+        TestLogBackExample logExecute = new TestLogBackExample();
+        logExecute.info("开始打印了。。");
+        logExecute.error("开始打印了。。");
+    }
 
 }
