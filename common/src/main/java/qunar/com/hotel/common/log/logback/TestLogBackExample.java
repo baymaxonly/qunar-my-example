977e0634aff285968b032dc4d96b6e6fb9aafdbc
543405693@qq.com <543405693@qq.com>
2016-12-22 14:06:56 +0800

    'tets'

--- common/src/main/java/qunar/com/hotel/common/log/logback/TestLogBackExample.java
+++ common/src/main/java/qunar/com/hotel/common/log/logback/TestLogBackExample.java
@@ -1,5 +1,6 @@
 package qunar.com.hotel.common.log.logback;
 
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,23 +8,19 @@ import org.slf4j.LoggerFactory;
  * Created by shixian.zhen on 2016/12/20.
  */
 
-
 public class TestLogBackExample {
 
     private Logger logger = LoggerFactory.getLogger(TestLogBackExample.class);
-
-    public void info (String printContent){
+    String printContent = "开始打印...";
+    @Test
+    public void info (){
         logger.info("logger level info {}", printContent);
     }
 
-    public void error (String printContent){
+    @Test
+    public void error (){
         logger.error("logger level error {}", printContent);
     }
 
-    public static void main(String args[]){
-        TestLogBackExample logExecute = new TestLogBackExample();
-        logExecute.info("开始打印了。。");
-        logExecute.error("开始打印了。。");
-    }
 
 }
